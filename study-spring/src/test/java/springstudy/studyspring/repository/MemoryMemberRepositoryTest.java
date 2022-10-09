d3c5204f4d79dc73b5fc95e7072764678591c729
Ray Lee <wooo519@naver.com>
2022-10-09 14:20:58 +0900

    feat : add member

---
+++ study-spring/src/test/java/springstudy/studyspring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package springstudy.studyspring.repository;
+
+import org.assertj.core.api.Assertions;
+import springstudy.studyspring.domain.Member;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void  findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+}
