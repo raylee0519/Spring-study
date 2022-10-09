e3b73981be8b5bb8260819131d06fe97eeec9828
Ray Lee <wooo519@naver.com>
2022-10-09 14:26:27 +0900

    feat : set test

--- study-spring/src/test/java/springstudy/studyspring/repository/MemoryMemberRepositoryTest.java
+++ study-spring/src/test/java/springstudy/studyspring/repository/MemoryMemberRepositoryTest.java
@@ -10,7 +10,13 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
-    MemberRepository repository = new MemoryMemberRepository();
+    // MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore(); // 각각 메소드가 실행끝날때 해주는 것
+    }
 
     @Test
     public void save(){
@@ -37,4 +43,20 @@ class MemoryMemberRepositoryTest {
 
         assertThat(result).isEqualTo(member1);
     }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
 }
