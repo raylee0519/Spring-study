230d8590b22da3c38c0ec8d2efc52629eadc55f0
Ray Lee <wooo519@naver.com>
2022-10-15 23:35:25 +0900

    feat : new toy project

---
+++ spring-demo-project/src/main/java/springproject_1/springdemoproject/SpringDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package springproject_1.springdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoProjectApplication.class, args);
+	}
+
+}
---
+++ spring-demo-project/src/test/java/springproject_1/springdemoproject/SpringDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package springproject_1.springdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
