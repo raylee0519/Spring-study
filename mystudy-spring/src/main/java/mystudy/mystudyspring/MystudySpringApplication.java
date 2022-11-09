15df5b96e5b7a9d341d20d879e56d375b7f3775c
Ray Lee <wooo519@naver.com>
2022-11-09 18:49:37 +0900

    새로운 프로젝트

---
+++ mystudy-spring/src/main/java/mystudy/mystudyspring/MystudySpringApplication.java
@@ -0,0 +1,13 @@
+package mystudy.mystudyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystudySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystudySpringApplication.class, args);
+	}
+
+}
---
+++ mystudy-spring/src/test/java/mystudy/mystudyspring/MystudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mystudy.mystudyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystudySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
